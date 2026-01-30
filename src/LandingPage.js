2ef3332822fa727ee507fe4e4d2fcfd54f66504a
BlackHat86 <suryansh.c05@gmail.com>
2026-01-30 10:21:12 +0530

    removed view all button

--- src/LandingPage.js
+++ src/LandingPage.js
@@ -166,12 +166,12 @@ const LandingPage = () => {
                   world.
                 </p>
               </div>
-              <Link
+              {/* <Link
                 to="/login"
                 className="flex items-center gap-2 font-bold text-gray-900 hover:text-[#5865F2] transition-colors"
               >
                 View All <ArrowRight size={20} />
-              </Link>
+              </Link> */}
             </div>
 
             {loading ? (
